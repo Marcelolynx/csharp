b0fda4fe20b34ab4d9f65c26076b9ba661c89843
marcelolynx <mco02jgp>
2023-03-02 10:58:49 -0400

    array functions

---
+++ FunctionConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+using FunctionConsole.functions;
+
+var person = new PersonName();
+
+var meuArray = new ArrayList(); // Todos os tipos de dados
+meuArray.Add("Marcelo");
+meuArray.Add("Oliveira");
+meuArray.Add(46);
+
+var arrayTipado = new int[3] {0, 2, 8};
+
+Console.WriteLine((string) meuArray[1] + ",");
+Console.WriteLine((string) meuArray[0]);
+
+Array.Resize(ref arrayTipado, 20);
+
+arrayTipado[19] = 20;
+
+Console.WriteLine(arrayTipado);
+
+foreach (var dados in meuArray)
+{
+    Console.WriteLine(dados);
+}
+foreach (var dados in arrayTipado)
+{
+    Console.WriteLine(dados);
+}
+Console.WriteLine(person);
